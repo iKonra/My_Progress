4d22d42d25024e3b16cb45744fb9d1f23d6bd108
Konra <82560729+iKonra@users.noreply.github.com>
2024-10-29 02:55:17 -0300

    Add files via upload

---
+++ POO/Binarios/Program 1 y 2/Program2.cpp
@@ -0,0 +1,29 @@
+// Escriba otro programa que lea el archivo generado por el primero para verificar si se escribieron correctament
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+struct par {
+	int x;
+	float y;
+};
+int main() {
+	
+	
+	ifstream archi("grupo.dat", ios::binary|ios::ate);
+	if(!archi) 
+		throw runtime_error("El archivo no se abrio correctamente.");
+	
+	int N = archi.tellg()/sizeof(par);
+	archi.seekg(0);
+	
+	for(int i=0;i<N;i++) {
+		par x;
+		archi.read(reinterpret_cast<char*>(&x),sizeof(x)) ;
+		cout << x.x << " " << x.y << endl;
+	}
+	
+	
+	
+	return 0;
+}
